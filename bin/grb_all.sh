ccd8969f79a869368102bbfe5fa7ba30b4f2f857
basti <12233784+sebastianseitz@users.noreply.github.com>
2017-05-31 16:21:30 +0200

    add alias for `--force-with-lease`

--- bin/grb_all.sh
+++ bin/grb_all.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
-for project in $(find -x /Users/seitzs/Development/ -type d -depth 1); do
+for project in $(find -H -x /Users/seitzs/Development/ -type d -depth 1); do
     base=$(basename ${project});
     if git -C /Users/seitzs/Development/${base}/ rev-parse --git-dir > /dev/null 2>&1; then
         echo '//////' ${base};
         git -C /Users/seitzs/Development/${base}/ checkout -b himself -q > /dev/null 2>&1;
         git -C /Users/seitzs/Development/${base}/ fetch;
         git -C /Users/seitzs/Development/${base}/ rebase origin/master;
+        echo ${base} '//////';
+        echo;
+    fi
+done;
+
+for project in $(find -H -x /Users/seitzs/.virtualenvs/ -type d -depth 1); do
+    base=$(basename ${project});
+    if git -C /Users/seitzs/.virtualenvs/${base}/src/${base}/ rev-parse --git-dir > /dev/null 2>&1; then
+        echo '//////' ${base};
+        git -C /Users/seitzs/.virtualenvs/${base}/src/${base}/ checkout -b himself -q > /dev/null 2>&1;
+        git -C /Users/seitzs/.virtualenvs/${base}/src/${base}/ fetch;
+        git -C /Users/seitzs/.virtualenvs/${base}/src/${base}/ rebase origin/master;
         echo ${base} '//////';
         echo;
     fi
